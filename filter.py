ffe63a98e48c11c7211e93f5b355a058f9971efa
Srinivas9362 <116152308+Srinivas9362@users.noreply.github.com>
2025-08-13 17:06:31 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,6 +1,6 @@
 from snowflake.snowpark.function import col
 
 
-def filter_by_column_values(session, table_name, column_value):
+def filter_by_column_value(session, table_name, column_value):
   df = session.table(table_name)
   return df.filter(col("COLUMN3") == column_value)
