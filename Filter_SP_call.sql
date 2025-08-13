2cd33424d59bf54c85c4ca668db6b20251d13e19
Srinivas9362 <116152308+Srinivas9362@users.noreply.github.com>
2025-08-13 17:08:50 +0530

    Update Filter_SP_call.sql

--- Filter_SP_call.sql
+++ Filter_SP_call.sql
@@ -2,7 +2,7 @@ create or replace procedure filter_by_column_value(table_name varchar, column_va
 
 returns table(id number, column1 number, column2 date, column3 text)
 language python
-runtime_version = '3.8'
+runtime_version = '3.9'
 packages = ('snowflake-snowpark-python')
 imports = ('@git_demo.public.my_github_public_repo/branches/main/filter.py')
 handler = 'filter.filter_by_column_value';
